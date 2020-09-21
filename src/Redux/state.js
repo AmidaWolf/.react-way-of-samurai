2591b68dd1c1dd51a34d0d9246d685667b407582
Yauhen <amidawolf@gmail.com>
2020-09-21 18:13:49 +0300

    refactor(37 lesson): rewrite state and index to oop

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,56 +1,60 @@
-let renderTree = () => {} //need for subscribe
+let store = {
+    _state: {
+        profilePage: {
+            postData: [
+                {id: 0, text: 'Hi everyone', likes: 5},
+                {id: 1, text: 'What\'s next?', likes: 3}
+            ],
+            newPostText: ''
+        },
+        messagesPage: {
+            textData: [
+                {name: 'Kus', text: 'Sup'},
+                {name: 'Kus', text: 'Where my money?'}
+            ],
+            usersDialogData: [
+                {id: 0, name: 'Kus Kabanov'},
+                {id: 1, name: 'Gus Kabanov'},
+                {id: 2, name: 'Srus Kabanov'}
+            ],
+        },
+        navigationBlock: {
+            onlineFriends: [
+                {id: 0, name: 'Kus Kabanov'},
+                {id: 1, name: 'Gus Kabanov'},
+            ]
+        }
 
-let state ={
-    profilePage: {
-        postData: [
-            {id: 0, text: 'Hi everyone', likes: 5},
-            {id: 1, text: 'What\'s next?', likes: 3}
-        ],
-        newPostText: ''
     },
-    messagesPage: {
-        textData: [
-            {name: 'Kus', text: 'Sup'},
-            {name: 'Kus', text: 'Where my money?'}
-        ],
-        usersDialogData: [
-            {id: 0, name: 'Kus Kabanov'},
-            {id: 1, name: 'Gus Kabanov'},
-            {id: 2, name: 'Srus Kabanov'}
-        ],
+    getState () {
+      return this._state;
     },
-    navigationBlock: {
-        onlineFriends: [
-            {id: 0, name: 'Kus Kabanov'},
-            {id: 1, name: 'Gus Kabanov'},
-        ]
-    }
+    _callSubscriber () {},
+    //need for subscribe
+    addPost: function () {
+
+        let lastElement = this._state.profilePage.postData.length;
+        let newID = lastElement + 1;
+
+        let newPost = {
+            id: {newID},
+            text: this._state.profilePage.newPostText,
+            likes: 0
+        }
+
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state)
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer;
+    }, //callback
 
 }
 
-export const addPost = () => {
-
-    let lastElement = state.profilePage.postData.length;
-    let newID = lastElement + 1;
-
-    let newPost = {
-        id: {newID},
-        text: state.profilePage.newPostText,
-        likes: 0
-    }
-
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostText = '';
-    renderTree(state);
-}
-
-export const updNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderTree(state)
-}
-
-export const subscribe = (observer) => {
-    renderTree = observer;
-} //callback
-
-export default state;
+export default store;
+window.store = store; //for global
--- src/index.js
+++ src/index.js
@@ -4,27 +4,25 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
-import state, {subscribe} from "./Redux/state";
+import store from "./Redux/state";
 import App from './App';
-import {addPost, updNewPostText} from "./Redux/state";
-
 
 let renderTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <App
                 state={state}
-                addPost={addPost}
-                updNewPostText={updNewPostText}
+                addPost={store.addPost.bind(store)} //bind for this always = store
+                updNewPostText={store.updNewPostText.bind(store)}
             />
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
 
-renderTree(state);
+renderTree(store.getState());
 
-subscribe(renderTree); //callback to state
+store.subscribe(renderTree); //callback to state
 
 
 // If you want your app to work offline and load faster, you can change
